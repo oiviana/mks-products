9ba49bbf3321cff420dcdad27ecbd383fa8f5658
oiviana <vianalucas80@gmail.com>
2024-05-09 20:08:42 -0300

    feat: footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import Products from "@/components/Products";
 import variables from "./styles/variables.module.scss";
 import Header from "@/components/Header";
 import Cart from "@/components/Cart";
+import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <>
       <Header />
       <Cart/>
       <Products />
+      <Footer/>
     </>
   );
 }
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.scss'
+
+export default function Footer(){
+    return(
+        <footer className={styles.footer}>
+           <span>MKS sistemas &copy; Todos os direitos reservados</span>
+        </footer>
+    )
+}
