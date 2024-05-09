f81b9b3454b1093c4990b08ca4cb5457b850468b
oiviana <vianalucas80@gmail.com>
2024-05-09 20:00:27 -0300

    feat: product card

---
+++ src/components/Products/ProductCard/index.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import styles from "../styles.module.scss";
+import AddToCart from "@/components/Cart/AddToCart";
+
+export default function ProductCard({name, price, description, photo,}:ProductProps){
+    const priceFormatter = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+ 
+      });
+  return (
+    <div className={styles.productCard}>
+      <div className={styles.imageWrapper}>
+        <Image alt={name} width={230} height={139} src={photo}  style={{objectFit:"contain"}}/>
+      </div>
+      <div className={styles.cardContent}>
+        <div className={styles.productTitle}>
+          <h3>{name}</h3>
+          <span> {priceFormatter.format(Number(price))}</span>
+        </div>
+        <span className={styles.productDescription}>{description}</span>
+        <AddToCart />
+      </div>
+    </div>
+  );
+}
