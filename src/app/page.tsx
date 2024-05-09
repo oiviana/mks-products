4ed8a9c872753c66e34556d3bb356a3584cf4f74
oiviana <vianalucas80@gmail.com>
2024-05-09 11:16:55 -0300

    test: saas modules

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
-import variables from './styles/variables.module.scss'
+import Products from "@/components/Products";
+import variables from "./styles/variables.module.scss";
 export default function Home() {
   return (
-   <h2 className={variables.test}>oi</h2>
+    <>
+      <h2 className={variables.test}>oi</h2>
+      <Products/>
+    </>
   );
 }
