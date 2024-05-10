f1c5fcf21fb0ea6d7f3d0be3ddeb49ab109f3807
oiviana <vianalucas80@gmail.com>
2024-05-10 16:55:21 -0300

    feat: add to cart function

--- src/components/Cart/AddToCart/index.tsx
+++ src/components/Cart/AddToCart/index.tsx
@@ -1,9 +1,17 @@
 import { LuShoppingBag } from "react-icons/lu";
 import styles from "../styles.module.scss";
+import { ProductProps } from "@/components/Products/types";
+import { useCart } from "@/contexts/CartContext";
 
-export default function AddToCart() {
+export default function AddToCart(productData: ProductProps) {
+  const { addItemToCart } = useCart();
+  const { id, name, photo, price } = productData;
+
   return (
-    <button className={styles.addToCart}>
+    <button
+      className={styles.addToCart}
+      onClick={() => addItemToCart(id, name, photo, price)}
+    >
       <LuShoppingBag size={14} />
       comprar
     </button>
