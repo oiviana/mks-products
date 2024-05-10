7fb9ae9c7350a3d24ae7b5bf0a369ab24f0558c8
oiviana <vianalucas80@gmail.com>
2024-05-10 16:54:22 -0300

    refactor: empty cart fallback

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -5,7 +5,9 @@ import { IoClose } from "react-icons/io5";
 import CartProduct from "../Products/CartProduct";
 
 export default function Cart() {
-  const { isCartOpen, closeCart } = useCart();
+  const { isCartOpen, closeCart, cartItems, totalPrice } = useCart();
+
+  const cartTotalPrice = totalPrice()
 
   return (
     <div
@@ -19,29 +21,34 @@ export default function Cart() {
           <IoClose size={25} />
         </button>
       </div>
-      <div className={styles.cartContent}>
-        <CartProduct name="APPlE" photo="/test.png" price="200" quantity={0} />
-        <CartProduct name="APPlE" photo="/test.png" price="200" quantity={0} />
-        <CartProduct name="APPlE" photo="/test.png" price="200" quantity={0} />
-        <CartProduct name="APPlE" photo="/test.png" price="200" quantity={0} />
-        <CartProduct name="APPlE" photo="/test.png" price="200" quantity={0} />
-        <CartProduct name="APPlE" photo="/test.png" price="200" quantity={0} />
-        <CartProduct name="APPlE" photo="/test.png" price="200" quantity={0} />
- 
-
-
-
-  
-
-  
-      </div>
-      <div className={styles.cartFooter}>
-        <div className={styles.cartFooterTotal}>
-          <span>Total:</span>
-          <span>R$798</span>
+      {cartItems.length === 0 ? (
+        <div className={styles.emptyCart}>
+          <h2>Seu carrinho está vazio!</h2>
+          <h2>Adicione produtos :)</h2>
         </div>
-        <button className={styles.cartBuy}>Finalizar Compra</button>
-      </div>
+      ) : (
+        <>
+          <div className={styles.cartContent}>
+            {cartItems.map((item) => (
+              <CartProduct
+                key={item.id}
+                name={item.name}
+                photo={item.photo}
+                price={item.price}
+                quantity={item.quantity}
+                id={item.id}
+              />
+            ))}
+          </div>
+          <div className={styles.cartFooter}>
+            <div className={styles.cartFooterTotal}>
+              <span>Total:</span>
+              <span>R$ {cartTotalPrice}</span>
+            </div>
+            <button className={styles.cartBuy}>Finalizar Compra</button>
+          </div>
+        </>
+      )}
     </div>
   );
 }
