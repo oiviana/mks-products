b12577418d31f590d259e00e6824a2d8f6b8f324
oiviana <vianalucas80@gmail.com>
2024-05-10 12:08:53 -0300

    types: cart props

--- src/components/Products/types.d.ts
+++ src/components/Products/types.d.ts
@@ -1,17 +1,20 @@
-interface ProductData{
-    data:{
-        count:number
-        products: ProductProps[]
-    }
+interface ProductData {
+  data: {
+    count: number;
+    products: ProductProps[];
+  };
 }
 interface ProductProps {
-    id: number;
-    name: string;
-    brand: string;
-    description: string;
-    photo: string;
-    price: string;
-    createdAt: string;
-    updatedAt: string;
+  id: number;
+  name: string;
+  brand: string;
+  description: string;
+  photo: string;
+  price: string;
+  createdAt: string;
+  updatedAt: string;
 }
 
+type CartProductProps =  {
+  quantity: number;
+} & Pick<ProductProps, "price" | "name" | "photo">; ;
