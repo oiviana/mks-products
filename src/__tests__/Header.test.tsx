9a64c4c2ee27e6eebd327592f0d4462a6cd28fa7
oiviana <vianalucas80@gmail.com>
2024-05-11 12:38:42 -0300

    refactor: header test

--- src/__tests__/Header.test.tsx
+++ src/__tests__/Header.test.tsx
@@ -2,12 +2,14 @@ import { render, screen } from "@testing-library/react";
 import Header from "@/components/Header";
 import CartProvider from "@/contexts/CartContext";
 
-it("Home should has Header", () => {
+describe("Header", () => {
+  it("Home should has Header", () => {
     render(
       <CartProvider>
         <Header />
       </CartProvider>
     );
 
-    expect(screen.getByText('mks')).toBeInTheDocument();
+    expect(screen.getByText("mks")).toBeInTheDocument();
   });
+});
