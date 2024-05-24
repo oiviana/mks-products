11f7b5a53e801bc131862b5d7e043005653bfc89
oiviana <vianalucas80@gmail.com>
2024-05-24 12:16:47 -0300

    refactor: logo

--- src/components/Header/Logo/index.tsx
+++ src/components/Header/Logo/index.tsx
@@ -3,7 +3,7 @@ import styles from '../styles.module.scss'
 export default function Logo() {
   return (
     <h1 className={styles.logo}>
-      <span className={styles.logoMks}>mks </span>Sistemas
+      <span className={styles.logoMks}>mks </span>Products
     </h1>
   );
 }
